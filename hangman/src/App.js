1924ed374dbdd3d608ac58afb30da000353dcb09
PavanS27 <48400643+PavanS27@users.noreply.github.com>
2020-09-16 16:04:25 +0530

    display word

--- hangman/src/App.js
+++ hangman/src/App.js
@@ -1,12 +1,28 @@
-import React from 'react';
+import React, { useState, useEffect } from "react";
+import "./App.css";
+import Figure from "./components/Figure";
+import Header from "./components/Header";
+import Word from "./components/Word";
+import WrongLetters from "./components/WrongLetters";
 
-import './App.css';
+const words = ["application", "programming", "interface", "wizard"];
+let selectedWord = words[Math.floor(Math.random() * words.length)];
+let playable = true;
+const correctLetters = [];
+const wrongLetters = [];
 
 function App() {
+  const [playable, setPlayable] = useState(true);
+  const [correctLetters, setCorrectLetters] = useState([]);
+  const [wrongLetters, setWrongLetters] = useState([]);
   return (
     <div>
-    <h1>Hello</h1>
-    
+      <Header />
+      <div className="game-container">
+        <Figure />
+        <WrongLetters />
+        <Word selectedWord={selectedWord} correctLetters={correctLetters} />
+      </div>
     </div>
   );
 }
